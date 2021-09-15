af9f8ee2ae2b11145a3a9ce6b55f4b07e24c4956
YifanShenSZ <yshen57@jhu.edu>
2021-09-15 13:55:35 -0400

    organize tests for easier test

--- test/test.sh
+++ test/test.sh
@@ -4,6 +4,13 @@ for directory in utility file linalg; do
     cd $directory/build
     rm -f test.exe
     cmake --build .
-    ./test.exe
-    cd ../..
+    cd ..
+    if [ -d input ]; then
+        cd input
+        ../build/test.exe
+        cd ../..
+    else
+        ./build/test.exe
+        cd ..
+    fi
 done
