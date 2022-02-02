8c71a22ad961ce61ae2deef79ba6f3c106b98f26
YifanShenSZ <yshen57@jhu.edu>
2022-02-02 11:17:43 -0500

    better cmake practice

--- include/CppLibrary/utility/file.hpp
+++ include/CppLibrary/utility/file.hpp
@@ -20,19 +20,19 @@ class file_error : public std::exception {
         const char * what() const throw();
 };
 
-// Get the file name from a path (no need to use this since c++17)
+// get the file name from a path (no need to use this since c++17)
 std::string GetFileName(std::string path, bool withExtension = true, char seperator = '/');
 
-// Get the prefix from a path (no need to use this since c++17)
+// get the prefix from a path (no need to use this since c++17)
 std::string GetPrefix(std::string path, char seperator = '/');
 
-// Get the number of lines in a file
+// get the number of lines in a file
 size_t NLines(const std::string & file);
 
-// Get the number of strings in a file
+// get the number of strings in a file
 size_t NStrings(const std::string & file);
 
-// Read a vector from file
+// read a vector from file
 std::vector<double> read_vector(std::ifstream & ifs);
 std::vector<double> read_vector(const std::string & file);
 
